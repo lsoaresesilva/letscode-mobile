9f4dc61d23b75c6df849ad43eeb7296b79e454cc
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-03-19 14:56:36 -0300

    Criado o estilo inicial da página ClosedQuestions

--- src/scenes/ClosedQuestions/index.js
+++ src/scenes/ClosedQuestions/index.js
@@ -1,10 +1,88 @@
-import React from 'react';
+/* eslint-disable no-unused-expressions */
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { getAllSubject } from '../../store/modules/subject/actions';
+import {
+  Container,
+  HeaderContainer,
+  Header,
+  HeaderItem,
+  ListView,
+  ListItem,
+  IconSequence,
+  IconSequenceNumber,
+  MetainfoTitle,
+  ContainerNumberQuestions,
+  IconQtd,
+  TextQtdQuestions,
+  HeaderItemAction,
+  HeaderItemTitle,
+  MetainfoContainer,
+} from './styles';
 
-export default () => {
+export default ({ navigation }) => {
+  const dispatch = useDispatch();
+
+  const loading = useSelector(state => state.subject.loading);
+  const subjects = useSelector(state => state.subject.subjects);
+
+  useEffect(() => {
+    dispatch(getAllSubject());
+  }, []);
+
+  if (loading) {
+    return (
+      <View>
+        <Text>carregando</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>Questões fechadas</Text>
-    </View>
+    <Container>
+      <HeaderContainer>
+        <Header>
+          <HeaderItemAction
+            onPress={() => {
+              navigation.navigate('Home');
+            }}
+          >
+            <HeaderItem>
+              <Icon name="chevron-left" size={20} color="#fff" />
+            </HeaderItem>
+          </HeaderItemAction>
+
+          <HeaderItemTitle>assuntos</HeaderItemTitle>
+          <HeaderItemAction>
+            <HeaderItem>
+              <Icon name="filter" size={20} color="#fff" />
+            </HeaderItem>
+          </HeaderItemAction>
+        </Header>
+      </HeaderContainer>
+      <ListView
+        data={subjects.subjects}
+        renderItem={({ item }) => (
+          <ListItem
+            onPress={() => {
+              navigation.navigate('ClosedQuestions');
+            }}
+            key={item.id}
+          >
+            <IconSequence>
+              <IconSequenceNumber>{item.sequencia}</IconSequenceNumber>
+            </IconSequence>
+            <MetainfoContainer>
+              <MetainfoTitle>{item.nome}</MetainfoTitle>
+              <ContainerNumberQuestions>
+                <TextQtdQuestions>Questões</TextQtdQuestions>
+              </ContainerNumberQuestions>
+            </MetainfoContainer>
+          </ListItem>
+        )}
+      />
+    </Container>
   );
 };
