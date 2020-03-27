f3be1d3ad012b52bf6449da5e55eb8fe29e36c3d
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-03-27 19:54:56 -0300

    Refatorando os font family da tipografia

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -1,7 +1,9 @@
 import { scaleFont } from './mixins';
 
 // FONT FAMILY
-export const FONT_FAMILY_REGULAR = 'OpenSans-Regular';
+export const FONT_FAMILY_LIGHT = 'Lato-Light';
+export const FONT_FAMILY_REGULAR = 'Lato-Regular';
+export const FONT_FAMILY_BOLD = 'Lato-Bold';
 
 // FONT WEIGHT
 export const FONT_WEIGHT_LIGHT = '300';
@@ -49,7 +51,7 @@ export const LINE_HEIGHT_SUBTITLE_2 = scaleFont(18.2);
 
 // FONT STYLE
 export const FONT_LIGHT = {
-  fontFamily: FONT_FAMILY_REGULAR,
+  fontFamily: FONT_FAMILY_LIGHT,
   fontWeight: FONT_WEIGHT_LIGHT,
 };
 export const FONT_REGULAR = {
@@ -57,6 +59,6 @@ export const FONT_REGULAR = {
   fontWeight: FONT_WEIGHT_REGULAR,
 };
 export const FONT_BOLD = {
-  fontFamily: FONT_FAMILY_REGULAR,
+  fontFamily: FONT_FAMILY_BOLD,
   fontWeight: FONT_WEIGHT_BOLD,
 };
